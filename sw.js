6683fa1dc5297a9ce3561521c90fbd7e4750177b
Ichsan-Abdalla <120088732+Ichsan-Abdalla@users.noreply.github.com>
2022-12-17 08:51:37 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const $versi = "v0.c";
+const $versi = "v0.1";
 const $caches = [
 	'./','./index.html','./style.css','./script.js','./logo.png','./icon.png','./1.jpg','./2.jpg','./3.jpg','./4.jpg','./5.jpg','./6.jpg','./7.jpg','./harga.jpg','./erwin.jpg','./ryan.jpg','./Poppins-Regular.woff'
 ];
@@ -11,13 +11,14 @@ self.addEventListener('install', e => {
 	);
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', e=>{
+  self.skipWaiting();
+  e.waitUntil(
 		caches.keys().then(keys => {
-			return Promise.all(keys 
-				.filter(key => key !== $versi)
-				.map(key => caches.delete(key))
-			).then(_=>{window.location.reload()})
+		  return Promise.all(keys.filter(key => key !== $versi).map(key => caches.delete(key))
+		  )
 		})
+	);
 });
 
 self.addEventListener('fetch', e => {

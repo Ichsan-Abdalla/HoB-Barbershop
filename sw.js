72f333b660ae1a4651c6585d628542be22fa4cb1
Ichsan-Abdalla <120088732+Ichsan-Abdalla@users.noreply.github.com>
2022-12-18 15:14:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const $versi = "hob-v0.01";
+const $versi = "hob-v0.1a";
 const $caches = [
 	'./','./index.html','./style.css','./script.js','./logo.png','./icon.png','./1.jpg','./2.jpg','./3.jpg','./4.jpg','./5.jpg','./6.jpg','./7.jpg','./harga.jpg','./erwin.jpg','./ryan.jpg','./Poppins-Regular.woff'
 ];
@@ -19,7 +19,6 @@ self.addEventListener('activate', e=>{
 		  )
 		})
 	);
-window.href = '.';
 });
 
 self.addEventListener('fetch', e => {

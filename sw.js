7cdea6e79498f1a717a0f55a8f7c9632ee252b36
Ichsan-Abdalla <120088732+Ichsan-Abdalla@users.noreply.github.com>
2022-12-18 13:23:44 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const $versi = "v0.01";
+const $versi = "hob-v0.1";
 const $caches = [
 	'./','./index.html','./style.css','./script.js','./logo.png','./icon.png','./1.jpg','./2.jpg','./3.jpg','./4.jpg','./5.jpg','./6.jpg','./7.jpg','./harga.jpg','./erwin.jpg','./ryan.jpg','./Poppins-Regular.woff'
 ];
@@ -9,10 +9,10 @@ self.addEventListener('install', e => {
 			return cache.addAll($caches);
 		})
 	);
+	self.skipWaiting();
 });
 
 self.addEventListener('activate', e=>{
-  self.skipWaiting();
   e.waitUntil(
 		caches.keys().then(keys => {
 		  return Promise.all(keys.filter(key => key !== $versi).map(key => caches.delete(key))

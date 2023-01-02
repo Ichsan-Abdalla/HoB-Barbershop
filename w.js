dcb81a517c0df88a43ee68c9b4aaa07c8b76bdfd
Ichsan-Abdalla <120088732+Ichsan-Abdalla@users.noreply.github.com>
2023-01-02 11:54:46 +0700

    Update w.js

--- w.js
+++ w.js
@@ -1,5 +1,5 @@
 postMessage({
   'kapster':[0,1],
   'situasi':[0,0],
-  'tutup':[1,'https://www.tokopedia.com/seru/referral/kupon-sekutu-decjan-1212/04326203aa3c20043513691']
+  'tutup':[0,'']
 });
